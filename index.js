e51c24e16015d2a0dca096f454d0c72230006ff8
Avaer Kazmer <a@modules.io>
2021-12-29 15:24:16 -0500

    Major update

--- index.js
+++ index.js
@@ -1,126 +1,114 @@
 import * as THREE from 'three';
 // import easing from './easing.js';
 import metaversefile from 'metaversefile';
-const {useApp, useFrame, useActivate, useLoaders, usePhysics, addTrackedApp, useDefaultModules, useCleanup} = metaversefile;
+const {useApp, useFrame, useAvatarInternal, useActivate, useLoaders, useScene, usePhysics, useCleanup} = metaversefile;
 
 const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
 
+const unFrustumCull = o => {
+  o.traverse(o => {
+    if (o.isMesh) {
+      o.frustumCulled = false;
+      // o.castShadow = true;
+      // o.receiveShadow = true;
+    }
+  });
+};
+
 export default () => {
   const app = useApp();
-  const physics = usePhysics();
+  const scene = useScene();
+  const Avatar = useAvatarInternal();
+  // const physics = usePhysics();
+
+  async function createAvatar(app) {
+    await app.setSkinning(true);
+    const {skinnedVrm} = app;
+    const avatar = new Avatar(skinnedVrm, {
+      fingers: true,
+      hair: true,
+      visemes: true,
+      debug: false,
+    });
+    // avatar[appSymbol] = app;
   
-  let activateCb = null;
+    unFrustumCull(app);
+  
+    return avatar;
+  }
+  
+  /* let activateCb = null;
   let frameCb = null;
   useActivate(() => {
     activateCb && activateCb();
   });
   useFrame(() => {
     frameCb && frameCb();
-  });
+  }); */
 
-  let physicsIds = [];
+  const subApps = [];
+  // let physicsIds = [];
+  let avatar = null;
   (async () => {
-    const u = `${baseUrl}chest.glb`;
-    let o = await new Promise((accept, reject) => {
-      const {gltfLoader} = useLoaders();
-      gltfLoader.load(u, accept, function onprogress() {}, reject);
+    // const u2 = `${baseUrl}tsumugi-taka.vrm`;
+    const u2 = `${baseUrl}rabbit.vrm`;
+    const m = await metaversefile.import(u2);
+    const vrmApp = metaversefile.createApp({
+      name: u2,
     });
-    const {animations} = o;
-    o = o.scene;
-    app.add(o);
-    
-    const dropObject = new THREE.Object3D();
-    dropObject.position.y = 0.5;
-    app.add(dropObject);
 
-    // app.updateMatrixWorld();
+    // vrmApp.contentId = u2;
+    // vrmApp.instanceId = getNextInstanceId();
+    // console.log('set app position', app.position.toArray().join(','));
+    vrmApp.position.copy(app.position);
+    vrmApp.quaternion.copy(app.quaternion);
+    vrmApp.scale.copy(app.scale);
+    vrmApp.updateMatrixWorld();
+    // vrmApp.name = 'npcVrm';
+    vrmApp.setComponent('physics', true);
+    await vrmApp.addModule(m);
+    scene.add(vrmApp);
+    subApps.push(vrmApp);
 
-    /* let baseMesh = null;
-    o.traverse(o => {
-      if (!baseMesh && o.isMesh && /base_container/i.test(o.name)) {
-        baseMesh = o;
-      }
-    }); */
-    const physicsId = physics.addGeometry(o);
-    physicsIds.push(physicsId);
-    
-    const mixer = new THREE.AnimationMixer(o);
-    const actions = animations.map(animationClip => mixer.clipAction(animationClip));
-    /* let maxDuration = -Infinity;
-    for (const animation of animations) {
-      maxDuration = Math.max(maxDuration, animation.duration);
-    } */
-    const startOffset = 1;
-    const endOffset = 2;
-    const dropOffset = 1;
-    activateCb = () => {
-      // console.log('got activate');
-      
-      for (const action of actions) {
-        action.reset();
-        action.play();
-        action.time = startOffset;
-      }
-
-      let timeAcc = 0;
-      let lastUpdateTime = Date.now();
-      let dropped = false;
-      function animate() {
-        const now = Date.now();
-        const timeDiff = (now - lastUpdateTime) / 1000;
-        lastUpdateTime = now;
-        
-        timeAcc += timeDiff;
-        if (!dropped && timeAcc >= dropOffset) {
-          const {moduleUrls} = useDefaultModules();
-          
-          const r = () => (-0.5+Math.random())*2;
-          const components = [
-            {
-              key: 'drop',
-              value: {
-                velocity: new THREE.Vector3(r(), 1+Math.random(), r())
-                  .normalize()
-                  .multiplyScalar(5)
-                  .toArray(),
-                angularVelocity: new THREE.Vector3(0, 0.001, 0)
-                  .toArray(),
-              },
-            },
-          ];
-          
-          // console.log('got loot components', srcUrl, components);
-          const p = addTrackedApp(
-            moduleUrls.silk,
-            app.position.clone()
-              .add(new THREE.Vector3(0, 0.7, 0)),
-            app.quaternion,
-            app.scale,
-            components
-          );
-          dropped = true;
-        }
-        if (timeAcc >= endOffset) {
-          // mixer.stopAllAction();
-          /* timeAcc = 0;
-          for (const action of actions) {
-            action.time = 0;
-          } */
-          frameCb = null;
-        } else {
-          mixer.update(timeDiff);
-          mixer.getRoot().updateMatrixWorld();
-        }
-      }
-      frameCb = animate;
-    };
+    avatar = await createAvatar(vrmApp);
   })();
+
+  app.getPhysicsObjects = () => {
+    const result = [];
+    for (const subApp of subApps) {
+      result.push(...subApp.getPhysicsObjects());
+    }
+    return result;
+  };
+  // window.getPhysicsObjects = app.getPhysicsObjects;
+
+  useFrame(({timestamp, timeDiff}) => {
+    if (avatar) {
+      const f = timestamp / 5000;
+      const s = Math.sin(f);
+      avatar.inputs.hmd.position.set(s * 2, avatar.height, 0);
+      avatar.setTopEnabled(false);
+      avatar.setBottomEnabled(false);
+      for (let i = 0; i < 2; i++) {
+        avatar.setHandEnabled(i, false);
+      }
+      // const timeDiffS = timeDiff / 1000;
+      avatar.update(timeDiff);
+    }
+  });
   
-  useCleanup(() => {
+  /* app.addEventListener('transformupdate', () => {
+    for (const physicsObject of physicsIds) {
+      _getPhysicsTransform(physicsObject.position, physicsObject.quaternion);
+      physics.setTransform(physicsObject);
+    }
+  }); */
+
+  /* useCleanup(() => {
     for (const physicsId of physicsIds) {
       physics.removeGeometry(physicsId);
     }
-  });
+  }); */
 
   return app;
 };
